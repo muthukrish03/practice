285919ba342693b05963da33b13058533e76e0fa
muthukrish03 <mkkrishnan1011@gmail.com>
2024-04-30 20:12:28 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,6 @@
+const header =React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},React.createElement("h1",
+{},"My name is Muthu Krishnan")))
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(header)
